49ad14aa990768f1be5aa03356f9a8fcc72aa547
Gennady Raster <raster.iosdev@gmail.com>
2021-11-28 23:09:29 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Gena Raster on 28.11.21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
