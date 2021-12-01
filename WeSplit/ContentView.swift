e34f62aab59ea79872fb9d3b2af1ffcd910ae976
Gennady Raster <raster.iosdev@gmail.com>
2021-12-01 22:27:11 +0300

    Picker, TextField, NavigationView

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeopleIndex = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+            VStack {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    .textFieldStyle(.roundedBorder)
+                    .keyboardType(.decimalPad)
+                
+                Picker("Select number of people", selection: $numberOfPeopleIndex) {
+                    ForEach(2..<20) {
+                        Text("\($0) people")
+                    }
+                }
+                .pickerStyle(.automatic)
+                
+                Picker("Tips", selection: $tipPercentage) {
+                    ForEach(tipPercentages, id: \.self) {
+                        Text($0, format: .percent)
+                    }
+                }
+                .pickerStyle(.segmented)
+                
+                Spacer()
+            }
             .padding()
+            .navigationTitle("WeSplit")
+        }
     }
 }
 
